e9019e4144f7e7dcdf972be7d3dd208285720b5d
Lwen90 <qq251279275@163.com>
2014-05-19 15:41:48 +0800

    Update removeExtraFreqTerms.R

--- removeExtraFreqTerms.R
+++ removeExtraFreqTerms.R
@@ -1,6 +1,6 @@
 removeExtraFreqTerms <- function(freqTerms.list){
 	len <- length(freqTerms.list);
-	for( i in 1 : ( len-1)){ #去掉出现相同的项
+	for( i in 1 : ( len-1)){ 
 	i.len <- length(freqTerms.list[[i]]);
 			for( j in (i+1) : len){
 				for(k in 1 : length(freqTerms.list[[j]])){
@@ -11,7 +11,7 @@ removeExtraFreqTerms <- function(freqTerms.list){
 		}
 	
 	len.after.remove <- c();
-	for( i in 1 : len){ #第一轮去掉出现相同的项，获取长度
+	for( i in 1 : len){ 
 		len.after.remove <- append(len.after.remove, freqTerms.list[[i]]);
 	}
 	len.after.remove <- sort(len.after.remove);
